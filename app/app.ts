deb572fb2d41044648e630961335f47dae51aa3a
danielisaacgeslin <danielisaacgeslin@gmail.com>
2016-12-23 16:15:18 -0300

    update

--- app/app.ts
+++ app/app.ts
@@ -9,14 +9,14 @@ const rh: ResponseHelper = new ResponseHelper();
 
 function get(event, context, callback) {
     dmCtrl.get(event, context, callback).then(
-        data => rh.success(data, event),
-        error => rh.error(error, event)
+        data => callback(rh.success(data, event)),
+        error => callback(rh.error(error, event))
     );
 }
 
 function post(event, context, callback) {
     dmCtrl.post(event, context, callback).then(
-        data => rh.success(data, event),
-        error => rh.error(error, event)
+        data => callback(rh.success(data, event)),
+        error => callback(rh.error(error, event))
     );
 }
